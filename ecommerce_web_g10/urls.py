2c18b4d0fb59c1811b087dd4805fad4042f8b28a
hiendv02102000 <dovanhien021000@gmail.com>
2022-04-08 09:49:02 +0700

    test admin django

--- ecommerce_web_g10/urls.py
+++ ecommerce_web_g10/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from home import views as home
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     #home
+    path('',home.get_home),
+    path('login/',auth_views.LoginView.as_view(template_name="./login.html"), name="login"),
+    path('logout/',auth_views.LogoutView.as_view(next_page='/'),name='logout'),
+    path('accounts/profile/',home.get_home),
     #book
 
     #clothes
