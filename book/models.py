42715dea0d157dd056971ef45ab4e4718b3005a0
hiendv02102000 <dovanhien021000@gmail.com>
2022-04-08 13:58:26 +0700

    complete admin_book

--- book/models.py
+++ book/models.py
@@ -31,12 +31,14 @@ class BookAuthor (models.Model):
     id  = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255,null=False)
     bio = models.CharField(max_length=255,null=False)
-    
+    def __str__(self):
+        return f"{self.name}({self.bio})"
 class Book_Author (models.Model):
     id  = models.AutoField(primary_key=True)
     Book = models.ForeignKey(Book, default = None, on_delete = models.CASCADE)
     Author = models.ForeignKey(BookAuthor, default = None, on_delete = models.CASCADE)
-
+    def __str__(self):
+        return f"{self.Book}({self.Author})"
 class ItemBook(models.Model):
     id  = models.AutoField(primary_key=True)
     Book = models.ForeignKey(Book, default = None, on_delete = models.CASCADE)
@@ -46,3 +48,5 @@ class ItemBook(models.Model):
     decription = models.CharField(max_length=255,null=False)
     inventory = models.CharField(max_length=255,null=False)
     is_for_sale = models.BooleanField(default=True,null=False)
+    def __str__(self):
+        return f"{self.Book}"
