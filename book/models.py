85e4af5ad3d4583b14de61b01c54edaa7a744e66
hiendv02102000 <dovanhien021000@gmail.com>
2022-04-11 22:45:27 +0700

    update img book

--- book/models.py
+++ book/models.py
@@ -2,6 +2,11 @@ from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class ImageBook (models.Model):
+    id  = models.AutoField(primary_key=True)
+    src = models.ImageField(null=False)
+    def __str__(self):
+        return f"{self.src}"
 class Publisher (models.Model):
     id  = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255,null=False)
@@ -25,6 +30,7 @@ class Book (models.Model):
     edition =  models.CharField(max_length=255,null=False)
     CategoryBook = models.ForeignKey(CategoryBook, default = None, on_delete = models.CASCADE)
     Publisher = models.ForeignKey(Publisher, default = None, on_delete = models.CASCADE)
+    Img = models.ForeignKey(ImageBook, default = None, on_delete = models.CASCADE,null=True)
     def __str__(self):
         return f"{self.title}({self.publication_date})"
 class BookAuthor (models.Model):
@@ -39,11 +45,7 @@ class Book_Author (models.Model):
     Author = models.ForeignKey(BookAuthor, default = None, on_delete = models.CASCADE)
     def __str__(self):
         return f"{self.Book}({self.Author})"
-class ImageBook (models.Model):
-    id  = models.AutoField(primary_key=True)
-    src = models.ImageField(max_length=255,null=False)
-    def __str__(self):
-        return f"{self.src}"
+
 class ItemBook(models.Model):
     id  = models.AutoField(primary_key=True)
     Book = models.ForeignKey(Book, default = None, on_delete = models.CASCADE)
