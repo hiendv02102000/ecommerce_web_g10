90e4e6bd56c53bb06f3ccb518e135d772e7071f2
unknown <ngochuy25120@gmail.com>
2022-04-11 16:44:45 +0700

    clothes

--- clothes/admin.py
+++ clothes/admin.py
@@ -1,3 +1,29 @@
+
 from django.contrib import admin
 
+
+from clothes.models import Clothes, CategoryClothes, ItemClothes
+
 # Register your models here.
+#Book
+class ClothesAdmin(admin.ModelAdmin):
+    # biểu diễn ra dạng bạng trên trang admin
+    list_display = ("name","brand","color","manufacturerDate","material","department","size","sizeCountry","washingType","weight","fitType","closureType","dimensions","countryOrigin","CategoryClothes")
+    # thanh search
+    search_fields = ["name"] 
+    # tìm theo tag
+    list_filter = ["CategoryClothes"]
+#đưa bảng lên trang admin
+admin.site.register(Clothes,ClothesAdmin)
+
+#CategoryBook
+class CategoryClothesAdmin(admin.ModelAdmin):
+    list_display = ("type",) 
+    search_fields = ["type"] 
+admin.site.register(CategoryClothes,CategoryClothesAdmin)
+
+#ItemBook
+class ItemClothesAdmin (admin.ModelAdmin):
+    list_display = ("Clothes","price","discount","isForSale")
+    search_fields = ["Clothes"]
+admin.site.register(ItemClothes,ItemClothesAdmin)
--- clothes/models.py
+++ clothes/models.py
@@ -1,3 +1,42 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class CategoryClothes (models.Model):
+    id  = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=255,null=False)
+    def __str__(self):
+        return f"{self.type}"
+
+class Clothes (models.Model):
+    id  = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255,null=False)
+    brand = models.CharField(max_length=255,null=False)
+    color = models.CharField(max_length=255,null=False)
+    manufacturerDate = models.DateField(null=False)
+    material = models.CharField(max_length=255,null=False)
+    department = models.CharField(max_length=255,null=False)
+    size = models.CharField(max_length=255,null=False)
+    sizeCountry = models.CharField(max_length=255,null=False)
+    washingType = models.CharField(max_length=255,null=False)
+    color = models.CharField(max_length=255,null=False)
+    weight = models.FloatField(null=True)
+    fitType = models.CharField(max_length=255,null=False)
+    closureType = models.CharField(max_length=255,null=False)
+    dimensions = models.CharField(max_length=255,null=False)
+    countryOrigin = models.CharField(max_length=255,null=False)
+    CategoryClothes = models.ForeignKey(CategoryClothes, default = None, on_delete = models.CASCADE)
+    def __str__(self):
+        return f"{self.name}({self.manufacturerDate})"
+
+class ItemClothes(models.Model):
+    id  = models.AutoField(primary_key=True)
+    Clothes = models.ForeignKey(Clothes, default = None, on_delete = models.CASCADE)
+    price = models.FloatField(null=True)
+    discount = models.FloatField(null=True)
+    promoText = models.CharField(max_length=255,null=False)
+    decription = models.CharField(max_length=255,null=False)
+    inventory = models.CharField(max_length=255,null=False)
+    isForSale = models.BooleanField(default=True,null=False)
+    def __str__(self):
+        return f"{self.Clothes}"
